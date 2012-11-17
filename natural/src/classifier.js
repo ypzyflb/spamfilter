f200d968b939d995c08e06121088d2c05c085869
ypzyflb <ypzyflb@gmail.com>
2012-11-17 15:41:35 -0800

    test committing

---
+++ natural/src/classifier.js
@@ -0,0 +1,10 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: hongyix
+ * Date: 11/14/12
+ * Time: 11:50 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+var natural = require ("natural");
+var classifier = new natural.BayesClassifier();
